319816acc918ac774d804b0eb83074849cc91397
mantielero <josemaria.alkala@gmail.com>
2023-09-24 16:20:01 +0200

    first commit

---
+++ dremel/main.py
@@ -0,0 +1,21 @@
+import cadquery as cq 
+from jupyter_cadquery import (
+    open_viewer, show, web_color,
+    set_defaults, get_defaults, 
+    PartGroup, Part, Faces, Edges
+)
+from cadquery_massembly import Mate, MAssembly, relocate
+
+# remove "clean" to avoid errors OCP kernel error
+cq.occ_impl.shapes.Shape.clean = lambda x: x
+
+cv = open_viewer("DiskArm", cad_width=640, height=500, theme="light")
+
+set_defaults(axes=True, axes0=True, mate_scale=4)
+
+from math import pi, sin, cos, sqrt, asin, degrees, radians
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+%matplotlib inline
